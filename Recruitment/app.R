7e9c2e28841a6c03cfff41947a1faa8ce4d79342
jcksac <richard_jackson3@hotmail.com>
2018-11-22 22:34:27 +0000

    ammended function

--- Recruitment/app.R
+++ Recruitment/app.R
@@ -8,22 +8,13 @@ rec.forcast<-function(N.site,rpm,open.rate,Max.Time,penal=0.5,plot=TRUE,...){
 
 
 ## Getting the number of open sites per month
-  open.site<-seq(1,N.site,by=open.rate) 
+open.site<-seq(1,N.site,by=open.rate) 
 if(max(open.site)!=N.site) open.site <- c(open.site,N.site)
 
-
-if(length(open.site)<Max.Time){
 open.site<-c(open.site,rep(N.site,Max.Time-length(open.site))) 
-
-} else {
-open.site <- open.site[1:Max.Time]
-warning("Not enough time to open all sites!")
-}
  
-open.site
-
 ### Basic average rate per site approach
-  month.rate<-open.site*rpm
+month.rate<-open.site*rpm
  
 ## penalisng monthly recruitment (recruits 1/2 as much in first month)
 penalty <- diff(c(0,month.rate))*penal
@@ -32,11 +23,11 @@ month.rate <- month.rate-penalty
 cum.rec<-round(cumsum(month.rate)) 
 month.rate <- diff(c(0,cum.rec))
 
-  rec<-data.frame("Monthly Rec"=month.rate,"Cumualtive Rec."=cum.rec) 
+rec<-data.frame("Monthly Rec"=month.rate,"Cumualtive Rec."=cum.rec) 
 
-  if(plot) plot(cum.rec,typ="l",xlab="Time (Months)",ylab="Cumulative Recruitment",font.lab=3,...) 
+if(plot) plot(cum.rec,typ="l",xlab="Time (Months)",ylab="Cumulative Recruitment",font.lab=3,...) 
 
-  return(rec) 
+return(rec) 
 
 } 
 
